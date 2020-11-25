33309be0c6aabfad96d17330df6bde9861b2f0d2
Jose Gabriel <jose.ortiz@methodo.cl>
2020-11-25 14:19:09 -0300

    Added sanitizer, operation proccesor

--- src/business/calculatorController.js
+++ src/business/calculatorController.js
@@ -3,9 +3,17 @@ const { multiplicationRequest } = require('../data/multiplicationRepository');
 const { divisionRequest } = require('../data/divisionRepository');
 const { subtractionRequest } = require('../data/subtractionRepository');
 
-const processOperation = (req) => {
-    for (let operation in req.body.operations) {
-        sendOperation(parseOperationToArray(req.body.operations[operation]));
+//TODO::VERIFICAR QUE LA OPERACION NO COMIENCE CON *, + o /
+
+
+const processOperation = async (operation) => {
+    let operations = parseOperationToArray(operation);
+    operations = sanitizeNegativeValues(operations);
+    let pivot = findPivot(operations);
+    while(pivot.operator) {
+        operations = await processOperationA(operations,pivot);
+        pivot = findPivot(operations);
+        console.log(operations);
     }
     return {code: httpOk, message: 'Queued'};
 };
@@ -24,16 +32,46 @@ const parseOperationToArray = (operation) => {
     return operationArray;
 };
 
-const sendOperation = (operation) => {
-    if (operation.indexOf('*') !== -1) {
-        multiplicationRequest('a','b');
-    } else if (operation.indexOf('/') !== -1) {
-        divisionRequest('a', 'b');
-    } else if (operation.indexOf('-') !== -1) {
-        subtractionRequest('c', 'b');
-    } else if (operation.indexOf('+') !== -1) {
-        sumRequest('a','b');
+const findPivot = (operation) => {
+    console.log(operation);
+    let indexMul = operation.indexOf('*');
+    let indexDiv = operation.indexOf('/');
+    let indexSub = operation.indexOf('-');
+    let indexSum = operation.indexOf('+');
+    let pivot = {operator: '', index: Infinity};
+    const notExists = -1;
+    if (indexMul !== notExists) {
+        pivot = {operator: '*', index: indexMul};
     }
+    if (indexDiv !== notExists && indexDiv < pivot.index) {
+        pivot = {operator: '/', index: indexDiv};
+    }
+    if (indexSub !== notExists && indexDiv === notExists && indexMul === notExists) {
+        pivot = {operator: '-', index: indexSub};
+    }
+    if (indexSum !== notExists && indexDiv === notExists && indexMul === notExists && indexSub === notExists) {
+        pivot = {operator: '+', index: indexSum};
+    }
+    if (pivot.index === Infinity) {
+        pivot = {operator: false, index: false}
+    }
+    return pivot;
+};
+
+const sanitizeNegativeValues = (operation) => {
+    if (!operation[0]) {
+        operation.splice(0, 2); //removes firsts 2 values from array
+        operation[0]= `-${operation[0]}`; // add negative symbol to the first element
+    }
+    return operation
+};
+
+const processOperationA = async (operations, pivot) => {
+    let toSolve = operations.splice(pivot.index-1, 3);
+    let result = eval(toSolve.join(''));
+    operations.splice(pivot.index-1,0,result);
+    resolve (operations);
+
 };
 
 module.exports = { processOperation };
---
+++ src/data/test.js
@@ -0,0 +1,18 @@
+const index = 1;
+const array = ['0','+','1','+','2','+','3','+','4'];
+
+//let operation = `${array[index-1]}${array[index]}${array[index+1]}`;
+//console.log(operation);
+//console.log(eval(operation));
+//console.log(array);
+
+let rem = array.splice(2,3);
+//array.splice(0, 1);
+//array[0]= `-${array[0]}`;
+//console.log(array);
+array.splice(2,0,88);
+//console.log(array);
+
+
+
+
